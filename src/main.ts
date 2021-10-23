511fc4e23eec8d55b9d0dd5a722960144f3ef3b7
Vilsafur <vilsafur@gmail.com>
2021-10-23 20:23:13 +0200

    feat: add Normalize.css +Remove About Page

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
+import 'normalize.css';
+import '@/styles/index.scss';
+
 createApp(App).use(router).mount("#app");
